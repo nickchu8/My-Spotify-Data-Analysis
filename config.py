d5a1183e1368641215498ea357706be979a1888c
Nick Chu <122839335+nickchu8@users.noreply.github.com>
2023-10-21 18:11:34 -0700

    Added files used to create csv files

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+Created on Wed Jan 29 10:29:55 2020
+
+@author: vlad
+"""
+
+#insert local parameters
+
+username = 'sillychubs'
+client_id ='5a93d42f0e7840f99f25e47634cce6c2'
+client_secret = 'e42261da20b4457186932b5154f1e463'
+redirect_uri = 'http://localhost:7777/callback'
+scope = 'user-read-recently-played'
